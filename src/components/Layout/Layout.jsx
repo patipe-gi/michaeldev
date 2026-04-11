934f2e0f15a142b9b2693a5862a8772f48408016
patipe michael <patipem56@gmail.com>
2026-04-11 17:46:00 +0100

    scrole to top

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import { useEffect } from 'react';
 
 const Layout = () => {
+   const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <div >
       <Header />    
